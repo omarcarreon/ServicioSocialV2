5fc72efc97825139a97c8db04302a48400dabe96
Angel González <angelgonzalez@Angels-MacBook-Pro.local>
2015-11-28 18:59:52 -0600

    icono, creditos

---
+++ CreditosTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CreditosTableViewController.h
+//  Móviles Servicio Social
+//
+//  Created by Angel González on 11/28/15.
+//  Copyright © 2015 Angel González. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreditosTableViewController : UITableViewController
+
+@end
